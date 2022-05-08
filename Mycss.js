dd022719051ace2120416c19eb2105878e8368f7
風 <caizaifa1314@gmail.com>
2022-05-08 12:20:11 +0800

    Update Mycss.js
    修改

--- Mycss.js
+++ Mycss.js
@@ -1,3 +1,13 @@
-$(function() {
-    $('span[data-tag=Feeling]').css('background', 'yellow')
+window.addEventListener('DOMContentLoaded', function() {
+    var jquery = document.createElement("script");
+    jquery.type = "text/javascript";
+    jquery.id = 'data-tag-jquery'
+    jquery.src = "https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js";
+    jquery.async = true;
+    document.getElementsByTagName("head")[0].appendChild(jquery);
 })
+window.setInterval(function() {
+    $(function() {
+        $('span[data-tag=Feeling]').css('background', 'yellow')
+    })
+}, 500)
